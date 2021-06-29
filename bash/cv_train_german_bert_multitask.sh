d0902a7e46fe496afb3897fdb610a02390d85b6f
subhadarship <subhadarshipanda08@gmail.com>
2021-06-29 08:26:38 -0400

    added bash for training using cross-validation data

---
+++ bash/cv_train_german_bert_multitask.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd ../src
+echo "WORKING DIR: $PWD"
+
+GPUID=1
+
+# German BERT
+
+MODELNAME=bert-base-german-cased
+
+MODELDIR=/mnt/backup/panda/GermEval2021/models
+
+# BERT parameters are frozen
+for HIDDIM in 128 256 512; do
+  for LR in 0.0005 0.005; do
+    for FOLD in fold_A fold_B fold_C fold_D fold_E; do
+
+      expt=cv_${FOLD}_multitask/${MODELNAME}/trg_fc${HIDDIM}_lr${LR}_frozen
+      CUDA_VISIBLE_DEVICES=${GPUID} python train.py --label_col_names Sub1_Toxic,Sub2_Engaging,Sub3_FactClaiming --eval_label_col_names Sub3_FactClaiming --tokenization ${MODELNAME} --model_name ${MODELNAME} --bert_fc_dim ${HIDDIM} --model_dir ${MODELDIR}/${expt} --log_file_path ../logs_cv/${expt}.txt --lr ${LR} --batch_size 1024 --max_epochs 999 --early_stopping_patience 10 --freeze_bert --train_data_dir ../data/cross_validation/${FOLD} --dev_data_dir ../data/cross_validation/${FOLD}
+      rm -rf ${MODELDIR}/${expt}
+
+    done
+  done
+done
