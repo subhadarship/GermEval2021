7c67077035309e477f3938e25027f1af7ebf0107
subhadarship <subhadarshipanda08@gmail.com>
2021-06-29 07:20:22 -0400

    added prepare cross validation data and analyze data notebook

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -1,5 +1,6 @@
 import os
 
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from data_utils import load_dataframe
@@ -32,3 +33,37 @@ if __name__ == "__main__":
     raw_test_data_df = load_dataframe(INP_TEST_DATA_PATH)
     raw_test_data_df.rename(columns={'c_text': 'comment_text'}, inplace=True)
     raw_test_data_df.to_csv(os.path.join(OUT_TEST_DIR, 'test.csv'), encoding='utf-8', index=False)
+
+    """5-fold cross validation data"""
+    CROSS_VALIDATION_DATA_DIR = os.path.join('../data/cross_validation')
+
+    # split into train and dev
+    part1, part2 = train_test_split(train_df, test_size=0.5,
+                                    stratify=train_df['Sub3_FactClaiming'],
+                                    random_state=123)
+    part1a, part1b = train_test_split(part1, test_size=0.5,
+                                      stratify=part1['Sub3_FactClaiming'],
+                                      random_state=123)
+    part2a, part2b = train_test_split(part2, test_size=0.5,
+                                      stratify=part2['Sub3_FactClaiming'],
+                                      random_state=123)
+
+    part0 = dev_df.copy()
+
+    train_dfs_dict = {
+        ('fold_A', 'dev'): part0,
+        ('fold_B', 'dev'): part1a,
+        ('fold_C', 'dev'): part1b,
+        ('fold_D', 'dev'): part2a,
+        ('fold_E', 'dev'): part2b,
+        ('fold_A', 'train'): train_df.copy(),
+        ('fold_B', 'train'): pd.concat([part0, part1b, part2a, part2b]),
+        ('fold_C', 'train'): pd.concat([part0, part1a, part2a, part2b]),
+        ('fold_D', 'train'): pd.concat([part0, part1a, part1b, part2b]),
+        ('fold_E', 'train'): pd.concat([part0, part1a, part1b, part2a]),
+    }
+
+    for (fold_name, split_name), dataframe in train_dfs_dict.items():
+        os.makedirs(os.path.join(CROSS_VALIDATION_DATA_DIR, fold_name), exist_ok=True)
+        dataframe.to_csv(os.path.join(CROSS_VALIDATION_DATA_DIR, fold_name, f'{split_name}.csv'), encoding='utf-8',
+                         index=False)
